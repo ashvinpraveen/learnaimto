b17a8c76db3bd9ed31b814a2c7a63c6560b62ad9
Ashvin Praveen <ashvinpraveen@gmail.com>
2026-08-01 04:24:40 +0800

    some basic cleanup

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: [".next/**", "node_modules/**", "next-env.d.ts"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs["recommended-latest"],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+);
